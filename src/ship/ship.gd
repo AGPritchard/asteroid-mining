28d4876617ffd485e554b6f33bf719a71a6eaa7f
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-11-30 17:37:47 +0000

    Add thruster particle emitters

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -14,14 +14,28 @@ func _physics_process(delta: float) -> void:
 	# handle velocity
 	if Input.is_action_pressed("thrust_forward"):
 		velocity = (Vector2.UP * thrust_strength).rotated(rotation)
+		$BackThruster.emitting = true
+	else:
+		$BackThruster.emitting = false
+	
 	if Input.is_action_pressed("thrust_backward"):
 		velocity = (Vector2.DOWN * thrust_strength).rotated(rotation)
+		$FrontThruster.emitting = true
+	else:
+		$FrontThruster.emitting = false
 
 	# handle rotation
 	if Input.is_action_pressed("thrust_left"):
 		rotation_direction -= 1.0
+		$LeftThruster.emitting = true
+	else:
+		$LeftThruster.emitting = false
+	
 	if Input.is_action_pressed("thrust_right"):
 		rotation_direction += 1.0
+		$RightThruster.emitting = true
+	else:
+		$RightThruster.emitting = false
 	
 	# movement
 	rotation += rotation_direction * rotational_thrust_strength * delta
