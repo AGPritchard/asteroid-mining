f58cd02a49f787eca464065f9800f94e777d3bee
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-12-01 12:06:38 +0000

    Refactor ship.gd

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -14,55 +14,80 @@ var acceleration := Vector2.ZERO
 var can_thrust := true
 var fuel := 100.0
 
+
+# --------------------
+# Built-in Functions
+# --------------------
 func _ready() -> void:
 	$ThrusterRebootTimer.wait_time = thruster_reboot_time
 
-func _physics_process(delta: float) -> void:
-	# reset move variable(s)
-	acceleration = Vector2.ZERO
-	
-	if fuel > 0:
-		if can_thrust:
-			# handle input
-			if Input.is_action_pressed("thrust_forward"):
-				acceleration = (Vector2.RIGHT * thrust_strength).rotated(rotation)
-				fuel -= fuel_delta
-				$BackThruster.emitting = true
-			else:
-				$BackThruster.emitting = false
-			
-			if Input.is_action_pressed("thrust_backward"):
-				acceleration = (Vector2.LEFT * thrust_strength).rotated(rotation)
-				fuel -= fuel_delta
-				$FrontThruster.emitting = true
-			else:
-				$FrontThruster.emitting = false
-			
-			if Input.is_action_pressed("thrust_left"):
-				acceleration = (Vector2.UP * thrust_strength).rotated(rotation)
-				fuel -= fuel_delta
-				$RightThruster.emitting = true
-			else:
-				$RightThruster.emitting = false
-			
-			if Input.is_action_pressed("thrust_right"):
-				acceleration = (Vector2.DOWN * thrust_strength).rotated(rotation)
-				fuel -= fuel_delta
-				$LeftThruster.emitting = true
-			else:
-				$LeftThruster.emitting = false
-	else:
+func _process(_delta: float) -> void:
+	# disable thrusters
+	if can_thrust and fuel <= 0:
 		# start thruster reboot timer
 		can_thrust = false
 		$ThrusterRebootTimer.start()
 		
-		# turn off all thrusters (only visual)
+		# turn off thruster particle emitters
 		$FrontThruster.emitting = false
 		$BackThruster.emitting = false
 		$LeftThruster.emitting = false
 		$RightThruster.emitting = false
 	
-	# handle looking
+	# replenish fuel
+	if fuel < 100.0:
+		fuel += fuel_replenishment_rate
+	
+	# emit signals
+	emit_signal("update_fuel", fuel)
+
+func _physics_process(delta: float) -> void:
+	# reset move variable(s)
+	acceleration = Vector2.ZERO
+	
+	if can_thrust:
+		_thrust_input()
+	_look()
+	
+	# apply velocity
+	velocity += acceleration * delta
+	velocity = move_and_slide(velocity)
+	
+
+# --------------------
+# Other Functions
+# --------------------
+func _thrust_input() -> void:
+	if Input.is_action_pressed("thrust_forward"):
+		acceleration = (Vector2.RIGHT * thrust_strength).rotated(rotation)
+		fuel -= fuel_delta
+		$BackThruster.emitting = true
+	else:
+		$BackThruster.emitting = false
+	
+	if Input.is_action_pressed("thrust_backward"):
+		acceleration = (Vector2.LEFT * thrust_strength).rotated(rotation)
+		fuel -= fuel_delta
+		$FrontThruster.emitting = true
+	else:
+		$FrontThruster.emitting = false
+	
+	if Input.is_action_pressed("thrust_left"):
+		acceleration = (Vector2.UP * thrust_strength).rotated(rotation)
+		fuel -= fuel_delta
+		$RightThruster.emitting = true
+	else:
+		$RightThruster.emitting = false
+	
+	if Input.is_action_pressed("thrust_right"):
+		acceleration = (Vector2.DOWN * thrust_strength).rotated(rotation)
+		fuel -= fuel_delta
+		$LeftThruster.emitting = true
+	else:
+		$LeftThruster.emitting = false	
+
+func _look() -> void:
+	# get looking input values
 	var vertical_look := Input.get_action_strength("look_backward") - Input.get_action_strength("look_forward")
 	var horizontal_look := Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
 	var look_direction := Vector2(horizontal_look, vertical_look)
@@ -72,16 +97,10 @@ func _physics_process(delta: float) -> void:
 		rotation = look_direction.angle()
 	else:
 		look_at(get_global_mouse_position())
-	
-	# apply velocity
-	velocity += acceleration * delta
-	velocity = move_and_slide(velocity)
-	
-	# replenish fuel
-	if fuel < 100.0:
-		fuel += fuel_replenishment_rate
-	
-	emit_signal("update_fuel", fuel)
 
+
+# --------------------
+# Signals
+# --------------------
 func _on_ThrusterRebootTimer_timeout() -> void:
 	can_thrust = true
