290f301c36a74b4b45615f0a813f86197c7cd4e3
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-12-01 06:51:12 +0000

    Add acceleration

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -4,39 +4,41 @@ export(float) var thrust_strength := 200.0
 export(float) var rotational_thrust_strength := 1.5
 
 var velocity := Vector2.ZERO
+var acceleration := Vector2.ZERO
 var rotation_direction := 0.0
 
 func _physics_process(delta: float) -> void:
 	# reset move variables
-	velocity = Vector2.ZERO
+	acceleration = Vector2.ZERO
 	rotation_direction = 0.0
 	
 	# handle velocity
 	if Input.is_action_pressed("thrust_forward"):
-		velocity = (Vector2.UP * thrust_strength).rotated(rotation)
+		acceleration = (Vector2.UP * thrust_strength).rotated(rotation)
 		$BackThruster.emitting = true
 	else:
 		$BackThruster.emitting = false
 	
 	if Input.is_action_pressed("thrust_backward"):
-		velocity = (Vector2.DOWN * thrust_strength).rotated(rotation)
+		acceleration = (Vector2.DOWN * thrust_strength).rotated(rotation)
 		$FrontThruster.emitting = true
 	else:
 		$FrontThruster.emitting = false
 
 	# handle rotation
 	if Input.is_action_pressed("thrust_left"):
-		rotation_direction -= 1.0
+		rotation_direction = -1.0
 		$LeftThruster.emitting = true
 	else:
 		$LeftThruster.emitting = false
 	
 	if Input.is_action_pressed("thrust_right"):
-		rotation_direction += 1.0
+		rotation_direction = 1.0
 		$RightThruster.emitting = true
 	else:
 		$RightThruster.emitting = false
 	
 	# movement
 	rotation += rotation_direction * rotational_thrust_strength * delta
+	velocity += acceleration * delta
 	velocity = move_and_slide(velocity)
