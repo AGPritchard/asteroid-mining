9a19c4239622c674bbcd8a3db844d4d6f323d3f5
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-12-01 07:10:57 +0000

    Add fuel bar ui

--- src/space/space.gd
+++ src/space/space.gd
@@ -1,4 +1,19 @@
 extends Node2D
 
+# TODO: add asteroids
+# TODO: add mining of asteroids
+# TODO: add cargo functionality
+# TODO: add drop off zone
+# TODO: add conversion of dropped off cargo to credit
+
 func _ready() -> void:
 	VisualServer.set_default_clear_color(Color8(4, 0, 20, 255))
+	
+	# connect signals
+	$Ship.connect("update_fuel", self, "_on_ship_update_fuel")
+
+func _process(_delta: float) -> void:
+	$FuelBar.set_global_position($Ship.global_position - Vector2(24, 50))
+
+func _on_ship_update_fuel(fuel: float) -> void:
+	$FuelBar.value = fuel
