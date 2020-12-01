3a629ea550839153dfd14b228cb564ecf31913f1
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-12-01 07:21:49 +0000

    Add fuel replenishment and thruster reboot timer

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -6,13 +6,18 @@ export(float) var thrust_strength := 200.0
 export(float) var rotational_thrust_strength := 1.5
 export(float) var fuel_delta := 0.75
 export(float) var fuel_rotational_factor := 0.5
-export(float) var fuel_replenishment_rate = 0.25
+export(float) var fuel_replenishment_rate := 0.25
+export(float) var thruster_reboot_time := 2
 
 var velocity := Vector2.ZERO
 var acceleration := Vector2.ZERO
 var rotation_direction := 0.0
 
+var can_thrust := true
 var fuel := 100.0
+
+func _ready() -> void:
+	$ThrusterRebootTimer.wait_time = thruster_reboot_time
 
 func _physics_process(delta: float) -> void:
 	# reset move variables
@@ -20,36 +25,42 @@ func _physics_process(delta: float) -> void:
 	rotation_direction = 0.0
 	
 	if fuel > 0:
-		# handle velocity
-		if Input.is_action_pressed("thrust_forward"):
-			acceleration = (Vector2.UP * thrust_strength).rotated(rotation)
-			fuel -= fuel_delta
-			$BackThruster.emitting = true
-		else:
-			$BackThruster.emitting = false
-		
-		if Input.is_action_pressed("thrust_backward"):
-			acceleration = (Vector2.DOWN * thrust_strength).rotated(rotation)
-			fuel -= fuel_delta
-			$FrontThruster.emitting = true
-		else:
-			$FrontThruster.emitting = false
+		if can_thrust:
+			# foward and backward input
+			if Input.is_action_pressed("thrust_forward"):
+				acceleration = (Vector2.UP * thrust_strength).rotated(rotation)
+				fuel -= fuel_delta
+				$BackThruster.emitting = true
+			else:
+				$BackThruster.emitting = false
+			
+			if Input.is_action_pressed("thrust_backward"):
+				acceleration = (Vector2.DOWN * thrust_strength).rotated(rotation)
+				fuel -= fuel_delta
+				$FrontThruster.emitting = true
+			else:
+				$FrontThruster.emitting = false
 
-		# handle rotation
-		if Input.is_action_pressed("thrust_left"):
-			rotation_direction = -1.0
-			fuel -= fuel_delta * fuel_rotational_factor
-			$LeftThruster.emitting = true
-		else:
-			$LeftThruster.emitting = false
-		
-		if Input.is_action_pressed("thrust_right"):
-			rotation_direction = 1.0
-			fuel -= fuel_delta * fuel_rotational_factor
-			$RightThruster.emitting = true
-		else:
-			$RightThruster.emitting = false
+			# handle rotation input
+			if Input.is_action_pressed("thrust_left"):
+				rotation_direction = -1.0
+				fuel -= fuel_delta * fuel_rotational_factor
+				$LeftThruster.emitting = true
+			else:
+				$LeftThruster.emitting = false
+			
+			if Input.is_action_pressed("thrust_right"):
+				rotation_direction = 1.0
+				fuel -= fuel_delta * fuel_rotational_factor
+				$RightThruster.emitting = true
+			else:
+				$RightThruster.emitting = false
 	else:
+		# start thruster reboot timer
+		can_thrust = false
+		$ThrusterRebootTimer.start()
+		
+		# turn off all thrusters (only visual)
 		$FrontThruster.emitting = false
 		$BackThruster.emitting = false
 		$LeftThruster.emitting = false
@@ -60,7 +71,11 @@ func _physics_process(delta: float) -> void:
 	velocity += acceleration * delta
 	velocity = move_and_slide(velocity)
 	
+	# replenish fuel
 	if fuel < 100.0:
 		fuel += fuel_replenishment_rate
 	
 	emit_signal("update_fuel", fuel)
+
+func _on_ThrusterRebootTimer_timeout() -> void:
+	can_thrust = true
