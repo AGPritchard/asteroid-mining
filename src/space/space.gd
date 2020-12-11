7ed9c2269cdc1811b92c663b27448a079d583ca9
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-12-11 00:30:55 +0000

    Add parallax background

--- src/space/space.gd
+++ src/space/space.gd
@@ -27,7 +27,7 @@ func _process(_delta: float) -> void:
 	$FuelBar.set_global_position($Ship.global_position - Vector2(24, 50))
 	
 	# TEMP:
-	$Dust.global_position = $Ship/Camera2D.global_position
+	#$Dust.global_position = $Ship/Camera2D.global_position
 
 # ----------------------------
 # Signal Funcions
